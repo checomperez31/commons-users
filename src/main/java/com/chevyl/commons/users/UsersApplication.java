037ed44af24e004accf62862ecd5dda2fe5fc4cd
checomperez31 <checomperez@gmail.com>
2021-12-13 20:11:58 -0600

    Initial commit

---
+++ src/main/java/com/chevyl/commons/users/UsersApplication.java
@@ -0,0 +1,9 @@
+package com.chevyl.commons.users;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class UsersApplication {}
